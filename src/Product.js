94e4a0b2f317150c45cb765f7cce6522075e10ca
sazzadtanim <tanimsazzad@gmail.com>
2022-07-25 00:02:34 +0600

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default function Product({name, address}) {
   return (
     <div className='product_container'>
-        <h3>{name}</h3>
-        <h2>$244</h2>
+        <h4>{name.toUpperCase()}</h4>
+        <h3>৳ 1,200</h3>
         <img src={address} alt='name'/>
+        <button>Add to cart</button>
     </div>
   )
 }
